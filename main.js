b01c21fa955e9a9bf923004cee4871987badb2ca
girish <girishnakate19@gmail.com>
2022-10-06 19:15:23 +0530

    getelementbyclassname

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-const form = document.querySelector("#form");
-const username = document.querySelector("#name");
-const email = document.querySelector("#email");
 const header = document.querySelector("#header-title");
 header.style.borderBottom = "solid 2px blue";
 
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if(username.value == "" || email.value == ""){
-        alert("Enter values!")
-    } else {
-        console.log(username.value, email.value);
-    }
-})
 const header2 = document.querySelector(".title");
 header2.style.fontWeight = "bold";
 header2.style.color = "green";
+
+const items = document.getElementsByClassName("list-group-item");
+console.log(items);
+items[2].style.backgroundColor = "green";
+
+for(let el of items){
+    el.style.fontWeight = "bold";
+}
