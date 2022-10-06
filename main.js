02f5a58f71e574b9cf2e2f401425d705e6b32984
girish <girishnakate19@gmail.com>
2022-10-06 19:51:44 +0530

    getelementbyclassname

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-const header = document.querySelector("#header-title");
-header.style.borderBottom = "solid 2px blue";
+// const header = document.querySelector("#header-title");
+// header.style.borderBottom = "solid 2px blue";
 
 
-const header2 = document.querySelector(".title");
-header2.style.fontWeight = "bold";
-header2.style.color = "green";
+// const header2 = document.querySelector(".title");
+// header2.style.fontWeight = "bold";
+// header2.style.color = "green";
+
+// const items = document.getElementsByClassName("list-group-item");
+// console.log(items);
+// items[2].style.backgroundColor = "green";
+
+// for(let el of items){
+//     el.style.fontWeight = "bold";
+// }
+
 
 const items = document.getElementsByClassName("list-group-item");
 console.log(items);
@@ -13,3 +22,10 @@ items[2].style.backgroundColor = "green";
 for(let el of items){
     el.style.fontWeight = "bold";
 }
+
+const li = document.getElementsByTagName("li");
+li[2].style.backgroundColor = "green";
+
+
+const li_class = document.getElementsByClassName("list-group-item");
+li_class[4].style.backgroundColor = "blue"; 
